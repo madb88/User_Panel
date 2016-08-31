fe77b6aab81ff49f66676589c401de6d46ae8149
Jakub <kaminskiqba@gmail.com>
2016-08-31 17:23:41 +0200

    Update bootstrap.php

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -219,9 +219,9 @@ Type::build('datetime')
  * Only try to load DebugKit in development mode
  * Debug Kit should not be installed on a production system
  */
-if (Configure::read('debug')) {
-    Plugin::load('DebugKit', ['bootstrap' => false]);
-}
+// if (Configure::read('debug')) {
+//     Plugin::load('DebugKit', ['bootstrap' => false]);
+// }
 
 Plugin::load('Migrations');
 
